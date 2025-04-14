e257db4f90143730e7232c71975a78751ebbcb7c
MarcelTryHard <o.marcel@escola.pr.gov.br>
2025-04-14 11:17:02 -0300

    js

---
+++ caloriasssss/calorias.js
@@ -0,0 +1,14 @@
+alert ("vamos fazer seu cadastro");
+
+var nome = prompt ("DIgite seu nome");
+console.log (nome)
+
+var email = prompt ("DIgite seu email");
+console.log (email)
+
+var senha = prompt ("DIgite sua senha");
+console.log (senha)
+
+var verificacao = prompt ("voce deseja ter selo verificado'");
+console.log (verificacao)
+
